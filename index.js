5c86f943b88d117924c94564fd72d30f668133b6
Himanshu Shivgotra <himanshushivgotra@gmail.com>
2024-12-27 18:43:42 +0530

    cors change

--- index.js
+++ index.js
@@ -16,9 +16,21 @@ const app = express();
 
 // Enable CORS with dynamic origin
 const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:5173'];
+// app.use(cors({
+//   // origin: (origin, callback) => {
+//   //   if (!origin || allowedOrigins.includes(origin)) {
+//   //     callback(null, true);
+//   //   } else {
+//   //     callback(new Error('Not allowed by CORS'));
+//   //   }
+//   // },
+//   origin:"*",
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,
+// }));
 app.use(
   cors({
-    origin: ['*'],
+    origin: '*',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     credentials: true
   })
