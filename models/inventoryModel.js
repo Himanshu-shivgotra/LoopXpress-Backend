d52583e5c1405a83f7ad010c2413319511c67a1c
Himanshu Shivgotra <himanshushivgotra.com>
2025-02-15 15:32:41 +0530

    change add-product

--- models/inventoryModel.js
+++ models/inventoryModel.js
@@ -1,43 +1,49 @@
 import mongoose from 'mongoose';
 
 const inventorySchema = new mongoose.Schema({
-  productId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
-    required: true
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    default: 0,
-    min: 0
-  },
-  locationWiseStock: [{
-    locationName: {
-      type: String,
-      required: true
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
     },
-    stock: {
-      type: Number,
-      required: true,
-      default: 0,
-      min: 0
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0,
+        min: 0
+    },
+    locationWiseStock: [{
+        locationName: {
+            type: String,
+            required: true
+        },
+        stock: {
+            type: Number,
+            required: true,
+            default: 0,
+            min: 0
+        }
+    },
+   ],
+    isWarehouseInventory:{
+        default: true,
+        type: Boolean,
     }
-  }],
-  lastUpdated: {
-    type: Date,
-    default: Date.now
-  }
+,
+    lastUpdated: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 // Add pre-save hook to calculate total quantity
-inventorySchema.pre('save', function(next) {
-  if (this.isModified('locationWiseStock')) {
-    this.quantity = this.locationWiseStock.reduce((total, location) => {
-      return total + location.stock;
-    }, 0);
-  }
-  next();
+inventorySchema.pre('save', function (next) {
+    if (this.isModified('locationWiseStock')) {
+        this.quantity = this.locationWiseStock.reduce((total, location) => {
+            return total + location.stock;
+        }, 0);
+    }
+    next();
 });
 
 export default mongoose.model('Inventory', inventorySchema);
